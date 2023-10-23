a770771e136ff71584dda960c959148fc681c5af
Iremkeser37 <145453393+Iremkeser37@users.noreply.github.com>
2023-10-23 13:54:33 +0300

    Add files via upload

---
+++ for dongüsü 3-/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace for_dongüsü_3_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnYaz = new System.Windows.Forms.Button();
+            this.lbSayılar = new System.Windows.Forms.ListBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // btnYaz
+            // 
+            this.btnYaz.Location = new System.Drawing.Point(23, 184);
+            this.btnYaz.Name = "btnYaz";
+            this.btnYaz.Size = new System.Drawing.Size(120, 39);
+            this.btnYaz.TabIndex = 1;
+            this.btnYaz.Text = "YAZ";
+            this.btnYaz.UseVisualStyleBackColor = true;
+            this.btnYaz.Click += new System.EventHandler(this.btnYaz_Click);
+            // 
+            // lbSayılar
+            // 
+            this.lbSayılar.FormattingEnabled = true;
+            this.lbSayılar.Location = new System.Drawing.Point(179, 42);
+            this.lbSayılar.Name = "lbSayılar";
+            this.lbSayılar.Size = new System.Drawing.Size(147, 290);
+            this.lbSayılar.TabIndex = 2;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(23, 117);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lbSayılar);
+            this.Controls.Add(this.btnYaz);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnYaz;
+        private System.Windows.Forms.ListBox lbSayılar;
+        private System.Windows.Forms.TextBox txtAd;
+    }
+}
+
---
+++ for dongüsü 3-/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_dongüsü_3_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+            lbSayılar.Items.Clear();
+            int sayi = Convert.ToInt32(txtAd.Text);
+            for(int i = 1; i < sayi; i++)
+            {
+                lbSayılar.Items.Add(i);
+            }
+        }
+    }
+}
